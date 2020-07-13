ca53a5f338ba07d1655d536df74195edbc600671
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-07-14 08:27:32 +0900

    chore(open-graph-scraper): update package

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ import * as ogs from 'open-graph-scraper';
 export class AppService {
   async getOGP(urlBase: string): Promise<object> {
     const url = encodeURI(urlBase);
-    const result = await ogs({ url });
-    return result.data;
+    const { result } = await ogs({ url });
+    return result;
   }
 }
